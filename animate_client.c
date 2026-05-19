f34ed145b71444844540b904c143619fb5870a2d
Asad Khalid <asadkhalid.2014@gmail.com>
2026-05-19 20:26:34 +1000

    fix: Moved disconnect logic inside if for client

--- animate_client.c
+++ animate_client.c
@@ -113,7 +113,9 @@ int main(int argc, char** argv, char** envp) {
         
         // check disconnect after the above if statement to first handle server repsonse
         if (strncmp(input_buffer, "Disconnect", 10) == 0) {
-            break;
+            close(fd_c2s);
+            close(fd_s2c);
+            return 0;
         }   
     }
     
